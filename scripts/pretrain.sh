b66cf9e905752949d9eea98418f0afabb38add30
A. Marincioni <s3442209@vibranium.liacs.nl>
2024-05-07 09:41:41 -0400

    fixed pretrain script changes

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -12,7 +12,7 @@ mkdir -p experiments
 mkdir -p experiments/seeds
 mkdir -p experiments/results
 
-EXP_NAME="pretraining_lag_llamatm"
+EXP_NAME="pretraining_lag_llama"
 FILENAME="experiments/seeds/${EXP_NAME}"
 CONFIGPATH="configs/lag_llama.json"
 
@@ -56,9 +56,9 @@ do
     -e $EXP_NAME -d "datasets" --seed $SEED \
     -r "experiments/results" \
     --batch_size 512 -m 1000 -n 128 \
-    --wandb_entity "maranc" --wandb_project "lag-llama-t" \
-    --all_datasets "australian_electricity_demand" "electricity_hourly" \
-    --test_datasets "australian_electricity_demand" \
+    --wandb_entity "jakobl" --wandb_project "lag-llama" \
+    --all_datasets "australian_electricity_demand" "electricity_hourly" "london_smart_meters_without_missing" "solar_10_minutes" "wind_farms_without_missing" "pedestrian_counts" "uber_tlc_hourly" "traffic" "kdd_cup_2018_without_missing" "saugeenday" "sunspot_without_missing" "exchange_rate" "cpu_limit_minute" "cpu_usage_minute" "function_delay_minute" "instances_minute" "memory_limit_minute" "memory_usage_minute" "platform_delay_minute" "requests_minute" "ett_h1" "ett_h2" "ett_m1" "ett_m2" "beijing_pm25" "AirQualityUCI" "beijing_multisite" "weather" \
+    --test_datasets "weather" "pedestrian_counts" "exchange_rate" "ett_m2" "platform_delay_minute" "requests_minute" "beijing_pm25" \
     --num_workers 2 --args_from_dict_path $CONFIGPATH --search_batch_size \
     --lr 0.0001
 done
